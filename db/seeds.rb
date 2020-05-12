dd7d3b97230893b24914263c26a6c1fae2cbf92c
Kubilay <kblycaglayan@gmail.com>
2020-05-12 11:14:43 +0300

    Seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'faker'
+require 'faker'
 
-# name1 = Faker::Name.name
-# email1 = Faker::Internet.email
-# password1 = 000000
-# User.create(name: name1, email: email1, password: password1, member_status: true)
+# User.create(name: 'Kubilay', email: 'a@a.com', password: '000000', member_status: true)
+# User.create(name: 'Marios', email: 'b@b.com', password: '000000', member_status: true)
 
-User.create(name: 'Kubilay', email: 'example@example.com', password: '000000', member_status: true)
-User.create(name: 'Marios', email: 'test@test.com', password: '000000', member_status: true)
-Post.create(body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", user_id: 1)
-Post.create(body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by ", user_id: 2)
+posts = ["Finally, one day, my friends convinced me to invite 
+  him to my birthday party. Because she also had a crush 
+  on him, too!","I've never felt SO STUPID in my entire life. 
+  He dropped me off at the bus station and, to avoid feeling like 
+  an even bigger loser, New Year's with him.","To this day, I 
+  feel stupid and  my dad, and even my grandma 
+  had all done stupid stuff after drinking too much. It is what 
+  it is, right?","Last year, I had a crush on this Paraguayan boy 
+  from college. I always tried to dress up for that class we had.",
+  "Until he said: 'Because I am in love with you, Miriã. I love you
+  and I've told you that thousands of times.' I was in shock","This one time,
+  a giant bouquet of flowers was delivered to the office 
+  with my name on it.","At the end of the day, I lost the bouquet 
+  and my man, because he thought I was cheating on him with 
+  somebody else.","And to show how upset I was, I peeled out 
+  with my car and ... drove STRAIGHT INTO A TREE that was in 
+  the middle of the parking lot.","But then, I tried to be cool 
+  and do it too, and I GOT STUCK! My dress was hanging up while 
+  my legs, panties, and tights were swinging, and my sister was 
+  trying to pull me off to save me.","I had been admiring this 
+  girl from a distance for months, because I had zero guts to 
+  go up and talk to her."]
+
+10.times do
+  p faker_name = Faker::Ancient.hero
+  p faker_email = Faker::Internet.email
+  faker_password = "000000"
+  p user = User.create(name: faker_name, email: faker_email, password: faker_password, member_status: true)
+  p Post.create(body: posts.sample, user_id: user.id)
+end
+
 
 
